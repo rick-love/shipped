ac4ddbf9eedecce486fb9339df8f6a2c19dff1c4
Dan Ramalho <Dramalho7@gmail.com>
2017-08-22 14:54:42 -0400

    models linked

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,4 @@
 class Job < ApplicationRecord
+	has_many :plane_jobs
+	has_many :planes, :through => :plane_jobs
 end
--- app/models/plane.rb
+++ app/models/plane.rb
@@ -1,2 +1,5 @@
 class Plane < ApplicationRecord
+	belongs_to :user , required: true
+	has_many :plane_jobs
+	has_many :jobs, :through => :plane_jobs
 end
