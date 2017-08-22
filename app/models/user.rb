2339ada0651e24f7033accfe4938bc072068945c
Richard Love <ricklove@outloook.com>
2017-08-22 17:06:24 -0400

    updated models and controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :planes
+
+  before_create :system_add_user
+
+  def system_add_user
+  	"useradd #{self.username}"
+  end
+
+  before_save :encrypt_password
+
+  def encrypt_password
+  self.password = BCrypt.encrypt(self.password, self.password_salt)
+end
+
+  validates :email, presence: true, message: "You need to enter an email."
+  validates :email, uniqueness: true
+
 end
